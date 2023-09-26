d9e7030574be37b70ccda670918cef96a96f79c1
root <root@runner-drneyaon-project-16870242-concurrent-08xpbf>
2023-09-26 11:35:58 +0200

    Release 2.1.24

--- includes/admin/class-wc-weareplanet-admin-settings-page.php
+++ includes/admin/class-wc-weareplanet-admin-settings-page.php
@@ -187,7 +187,7 @@ class WC_WeArePlanet_Admin_Settings_Page extends WC_Settings_Page {
 		$settings = array(
 			array(
 				'links' => array(
-					'https://plugin-documentation.weareplanet.com/weareplanet/woocommerce/2.1.23/docs/en/documentation.html' => __( 'Documentation', 'woo-weareplanet' ),
+					'https://plugin-documentation.weareplanet.com/weareplanet/woocommerce/2.1.24/docs/en/documentation.html' => __( 'Documentation', 'woo-weareplanet' ),
 					'https://www.weareplanet.com/contact/sales' => __( 'Sign Up', 'woo-weareplanet' ),
 				),
 				'type'  => 'weareplanet_links',
