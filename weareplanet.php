13586d23b0dc6265199c19c7447e92f4d4e13522
andrewrowanwallee <andrew.rowan@wallee.com>
2026-03-09 11:51:40 +0100

    Release 3.4.2

--- weareplanet.php
+++ weareplanet.php
@@ -3,7 +3,7 @@
  * Plugin Name: WeArePlanet
  * Plugin URI: https://wordpress.org/plugins/woo-weareplanet
  * Description: Process WooCommerce payments with WeArePlanet.
- * Version: 3.4.1
+ * Version: 3.4.2
  * Author: Planet Merchant Services Ltd
  * Author URI: https://www.weareplanet.com
  * Text Domain: woo-weareplanet
@@ -51,7 +51,7 @@ if ( ! class_exists( 'WooCommerce_WeArePlanet' ) ) {
 		 *
 		 * @var string
 		 */
-		private $version = '3.4.1';
+		private $version = '3.4.2';
 
 		/**
 		 * The single instance of the class.
